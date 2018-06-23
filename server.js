b298e61e41073e69f62b18152dcb6afa4447a613
welqueresser <welquer.esser@moduscreate.com>
2018-06-23 02:58:44 -0300

    Just testing Jenkins integration.

--- server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic("www")).listen(8100, function(){
-    console.log('Server running on 8100...');
-});
